032169a49a4a549c61abf96d59a9532387bc2026
pcJitsu <antonios.zikos1@gmail.com>
2023-07-02 21:31:35 -0500

    Placed appropriate refs for js and css, trying to add lsitener

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+var saveButtonEl = $("#saveBtn");
+
+saveButtonEl.on("click", function () {
+  console.log("Test");
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -21,3 +25,17 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
