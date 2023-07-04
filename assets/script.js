4420bbd1d13b71d401ff5dbc59b567c5b204082e
pcJitsu <antonios.zikos1@gmail.com>
2023-07-04 08:54:20 -0500

    Set up psuedo and current hour

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,33 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+// Get Current Time
 var currentHour = dayjs().format("D/MM/YYYY/HA");
 $("#currentDay").text(currentHour);
 
-$(document).ready(function () {
-  //Ensure HTML has been parsed.
+//Set Start Time
+var startTime = dayjs().startOf("day").hour(8);
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+//Create Loop That Fills Out Scheduler
+
+//Get HTML Loaded and Added Event Listner to Save Button
+$(document).ready(function () {
   var saveButtonEl = $(".saveBtn");
   saveButtonEl.on("click", function () {
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+    console.log("TEST");
   });
 });
 
+// Below is Code to Re-Create. Changed ID to Hour-Block from HTML Source
+{
+  /* <div id="hour-Block" class="row time-block past">
+<div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+<textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+<button class="btn saveBtn col-2 col-md-1" aria-label="save">
+  <i class="fas fa-save" aria-hidden="true"></i>
+</button>
+</div> */
+}
 // PSUEDO
-// display today targeting current day
+// display today targeting current day (DONE)
+//figure out start time (Done)
 // create loop to create different time blocks
 // change class on each segment based on current time minus block time
 // event listener on save saveButton
@@ -50,3 +46,28 @@ $(document).ready(function () {
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
+
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+
+//Ensure HTML has been parsed.
+
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+
+// TODO: Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour. HINTS: How can the id
+// attribute of each time-block be used to conditionally add or remove the
+// past, present, and future classes? How can Day.js be used to get the
+// current hour in 24-hour time?
+//
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
+// TODO: Add code to display the current date in the header of the page.
