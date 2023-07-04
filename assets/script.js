a8535fbc14af5e7745be0c9b40018d10ac50ca5e
pcJitsu <antonios.zikos1@gmail.com>
2023-07-04 16:34:26 -0500

    Just need to figure out how to retrieve time desc and fill text area

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,7 @@ var endOfWorkHours = 18;
 //create empty schedule hour variable
 var scheduleHour;
 var timeStatus;
-
+var timeDesc = "";
 // Get Current Time and Display
 var currentTime = dayjs().format("D/MM/YYYY/HA");
 $("#currentDay").text(currentTime);
@@ -25,14 +25,15 @@ function createSchedule() {
     } else {
       timeStatus = "future";
     }
-
+    //creating div block with all relevent information
     var appendBlock = `<div id="hour-${hour}" class="row time-block ${timeStatus}">
     <div class="col-md-1 hour">${hour}</div>
-    <textarea class="col-md-10 description"></textarea>
+    <textarea class="col-md-10 description">${timeDesc}</textarea>
     <button class="btn saveBtn col-md-1" aria-label="save">
         <i class="fas fa-save"></i>
     </button>
 </div>`;
+
     //appends block to parent container during each loop
     $(".container-lg").append(appendBlock);
   }
@@ -40,17 +41,17 @@ function createSchedule() {
 
 //Function save information when placed into textfield
 function saveEvents() {
-  console.log($(this).parent());
-  var timeBlock = $(this).parent();
-  var timeDesc = $(this).parent();
-
+  //this being saved button, traversing up, then back down to appropriate value
+  var timeBlock = $(this).parent().children().eq(1);
+  var timeDesc = $(this).parent().children().eq(1).val();
   localStorage.setItem(timeBlock, timeDesc);
-  console.log(localStorage.getItem(timeBlock));
+  console.log(timeBlock, timeDesc);
 }
-createSchedule();
 
 //Get HTML Loaded and Added Event Listner to Save Button
 $(document).ready(function () {
   var saveButtonEl = $(".saveBtn");
   saveButtonEl.on("click", saveEvents);
 });
+
+createSchedule();
